42c8f6f738ce16af36a8811b6f39d6bbf69b944e
shrutamchalotra <75723938+shrutamchalotra@users.noreply.github.com>
2022-09-07 11:15:39 +0530

    LAB ACTIVITY 1 (07-09-2022)

---
+++ LAB ACTIVITY 1 (07-09-2022) ARITHEMATIC OPERATIONS (1).py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[58]:
+
+
+x=5;
+
+
+# In[59]:
+
+
+y=2;
+
+
+# In[ ]:
+
+
+#Addition Operator ( + )
+
+#The addition operator is used to add two numbers. It is placed between two numbers that are to be added.
+#Syntax : number1 + number2
+
+#program
+
+
+# In[60]:
+
+
+print('x+y=',x+y)
+
+
+# In[ ]:
+
+
+#Subtraction Operator ( - )
+
+#The subtraction operator is used to subtract two numbers. It is placed between two numbers that are to be subtracted. The right placed number is subtracted from the one that is placed at left.
+#Syntax : number1 - number2
+
+#Program
+
+
+# In[61]:
+
+
+print('x-y=',x-y)
+
+
+# In[ ]:
+
+
+#Multiplication Operator ( * )
+
+#The multiplication operator is used to multiply two numbers. It is also placed between the two numbers that are to be operated.
+#Syntax : number1 * number2
+
+#Program
+
+
+# In[62]:
+
+
+print('x*y=',x*y)
+
+
+# In[ ]:
+
+
+#ivision Operator ( / )
+
+#The division operator is used to divide two numbers. It is used between the numbers that are to be operated.
+#Syntax : number1 / number2
+    
+#Program
+
+
+# In[63]:
+
+
+print('x/y=',x/y)
+
+
+# In[ ]:
+
+
+#Modulus Operation ( % )
+I
+#t is used to give out the remainder of a division operation. It is also placed between numbers. The right placed number divides the one on the left and the remainder is given as output.
+#Syntax : number1 % number2
+
+#Program
+
+
+# In[64]:
+
+
+print('x%y=',x%y)
+
+
+# In[ ]:
+
+
+#Exponent Operation ( ** )
+
+#It is used to perform exponential calculations. The right placed number acts as the power.
+#Syntax : number1* *number2
+
+#Program
+
+
+# In[65]:
+
+
+print('x**y=',x**y)
+
+
+# In[ ]:
+
+
+#Floor Division Operator ( // )
+
+#It is used to perform floor division. This gives the result in int format.
+#Syntax : number1 // number2
+
+#Program
+
+
+# In[66]:
+
+
+print('x//y=',x//y)
+
---
+++ LAB ACTIVITY 1 (07-09-2022) DATA TYPES.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#List Mutable
+
+#It is a collection of data in which date is stored in ordered form.
+#Syntax : object_name = [ elements ]
+#The above mentioned syntax is of list where the elements are mentioned in square brackets.
+
+#Program
+
+
+# In[10]:
+
+
+thislist=["apple", "mango", "banana", "apple"]
+print(thislist)
+
+
+# In[3]:
+
+
+#Tuple Immutable
+
+#A tuple is a collection of data which is mostly similar to a list except that it is immutable i.e. the value once stored in a tuple cannot be changed. Though tuple allows concatenation. Syntax : object_name = ( elements )
+#This is the syntax of a tuple, where elements are placed under parenthesis.
+
+#Program
+
+
+# In[8]:
+
+
+thistuple=("apple", "banana", "cherry", "apple")
+print(thistuple)
+
+
+# In[11]:
+
+
+#Dictionary Mutable
+
+#Dictionary is a collection of data which is not sequential like a list, thus indexing is not possible. It consists of a key and its value.
+#Example : dict1 = {1 : "Red", 2 : "Blue", 3 : "Green"}
+#The above syntax is for a dictionary where the elements are placed under curly brackets.
+
+#Program
+
+
+# In[12]:
+
+
+
+thisdict={"apple", "banana", "cherry", "apple"}
+print(thisdict)
+
